d9becc7dca8b03f6ed9ccfc3ae11e1bd25e6f192
sangitasarkar74 <sangitasarkar@gmail.com>
2023-01-29 23:12:49 +0530

    changes done

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,18 @@
 const todoReducer = (state, action) => {
-    switch (action.type) {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.payload];
+      return state;
 
-    }
-}
+    case "DELETE":
+      return state.filter((item) => {
+        if (item.id != action.payload.id) {
+          return state;
+        }
+      });
+    default:
+      return state;
+  }
+};
 
-export { todoReducer }
+export { todoReducer };
